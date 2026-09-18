a671beb466f6cbe3bb72a03960a2612696c8c528
Ishikabansal567 <ishikabansal1511@gmail.com>
2026-09-18 11:27:47 +0530

    Initial comit

---
+++ backend/src/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASSWORD exists:", !!process.env.DB_PASSWORD);
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "SafeCase API is running"
+    });
+});
+
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+
+        res.json({
+            message: "Database connected successfully",
+            time: result.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database connection failed"
+        });
+    }
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`SafeCase server running on port ${PORT}`);
+});
